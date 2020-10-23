df6b24f4c53852d858b7b320782ffbb420529d47
jashmakhija <jashmakhija06@gmail.com>
2020-10-24 04:40:41 +0530

    Started

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import index
+from .views import index,about,contact,tracker,search,searchlocation,productview,checkout
 
 urlpatterns = [
     path("", index, name="ShopHome"),
+    path("about/", about, name="AboutUs"),
+    path("contact/", contact, name="ContactUs"),
+    path("tracker/", tracker, name="TrackingStatus"),
+    path("search/", search, name="Search"),
+    path("searchlocation/", searchlocation, name="SearchLocation"),
+    path("productview/", productview, name="ProductView"),
+    path("checkout/", checkout, name="Checkout"),
 ]
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request,"shop/index.html")
+    products = Product.objects.all()
+    
+    n= len(products)
+    nslides =n//4 +ceil((n/4)-(n//4))
+    params={'no_of_slides': nslides, 'range': range(1,nslides),'product': products} 
+    return render(request ,"shop/index.html",params)
+
+def about(request):
+    return render(request, "shop/about.html")
+
+def contact(request):
+    return render(request, "shop/contact.html")
+
+def tracker(request):
+    return render(request, "shop/tracker.html")
+
+def search(request):
+    return render(request, "shop/search.html")
+
+def searchlocation(request):
+    return render(request, "shop/contact.html")
+
+def productview(request):
+    return render(request, "shop/productview.html")
+
+def checkout(request):
+    return render(request, "shop/checkout.html")
